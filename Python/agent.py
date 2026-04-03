67911c9dc02ed279d58d61a4f1f8019c5cd9d883
Nekrasov Vladimir <nekraVM@yandex.ru>
2026-04-04 02:06:42 +0300

    Added pycache to ignore, added agent entity, added folders to store keys, main

--- Python/agent.py
+++ Python/agent.py
@@ -1,3 +1,39 @@
+import sys
+import os
+
+import paramiko
+from paramiko import agent
+
+def save_private_key(ip:str, key):
+    with open(os.path.join("keys", "private", ip.replace('.', '_')), "w") as f:
+        key.write_private_key(f)
+
+def save_public_key(ip:str, key):
+    with open(os.path.join("keys", "public", ip.replace('.', '_')), "w") as f:
+        f.write(key.get_base64())
+
 class Agent(object):
-    def __init__(self):
-        pass
+    def __init__(self, ip):
+        self.ip = ip
+        self.ssh_client = None
+
+        key = paramiko.RSAKey.generate(4096)
+        save_private_key(ip, key)
+        save_public_key(ip, key)
+
+    def open_ssh_connection(self):
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        key = paramiko.RSAKey.from_private_key_file(os.path.join("keys", "private", self.ip.replace('.', '_')))
+
+        print("trying to connect to %s" % self.ip)
+        self.ssh_client.connect(
+            hostname=self.ip,
+            username="AgentPi",
+            pkey=key,
+            port=22,
+            timeout=10,
+        )
+        print("Successfully connected to %s" % self.ip)
+
--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,5 @@
-print('Hello, world!')
+from agent import Agent
+import paramiko
+
+agent = Agent("192.168.0.10")
+agent.open_ssh_connection()
